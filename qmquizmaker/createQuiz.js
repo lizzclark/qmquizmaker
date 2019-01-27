6c5a211bb8ff751cec78a85a7aac6552a17462f6
lizzclark <erclark13@outlook.com>
2019-01-27 09:28:35 +0000

    promisify everything in createQuiz

--- qmquizmaker/createQuiz.js
+++ qmquizmaker/createQuiz.js
@@ -1,71 +1,64 @@
-const fs = require("fs");
-const fse = require("fs-extra");
-const child_process = require("child_process");
+const fs = require('fs');
+const { promisify } = require('util');
+const child_process = require('child_process');
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const exec = promisify(child_process.exec);
 
 function createQuiz(fileName) {
+  const quizName = fileName.slice(0, -4);
   // read file and format data for inquirer
-  fse
-    .readFile(`./${fileName}`, "utf8")
+  readFile(`./${fileName}`, 'utf8')
     .then(fileContents => {
       // remove initial comment, ##title##, ##end##, and split questions into array
       const fileTextArray = fileContents
-        .split("##\n\n")[1]
-        .split("\n\n")
+        .split('##\n\n')[1]
+        .split('\n\n')
         .slice(0, -1);
       // make each question into an inquirer prompt object
       const questionObjects = fileTextArray.reduce((acc, questionText) => {
-        const lines = questionText.split("\n");
+        const lines = questionText.split('\n');
         const correctAnswerLetter = lines[lines.length - 1].slice(-1);
         const correctAnswerText = lines.slice(1, -1).filter(line => {
           return line[0] === correctAnswerLetter;
         })[0];
         acc.push({
           correctAnswerText,
-          type: "list",
+          type: 'list',
           name: lines[0],
-          choices: lines.slice(1, lines.length - 1)
+          choices: lines.slice(1, lines.length - 1),
         });
         return acc;
       }, []);
       return questionObjects;
     })
     .then(questionsData => {
-      // populate skeleton file with our inquirer question objects
-      fs.readFile(
-        `${__dirname}/skeleton.txt`,
-        "utf8",
-        (err, skeletonString) => {
-          if (err) console.log(err);
-          else {
-            const populatedString = skeletonString.replace(
-              "PLACEHOLDER1",
-              JSON.stringify(questionsData)
-            );
-            // write full quiz code to file
-            const quizName = fileName.slice(0, -4);
-            fs.writeFile(
-              `${process.cwd()}/${quizName}.js`,
-              populatedString,
-              err => {
-                if (err) console.log(err);
-                // use child process to install inquirer in working directory
-                else
-                  child_process.exec(`npm i inquirer`, null, err => {
-                    if (err) console.log(err);
-                    else {
-                      console.log(
-                        `Succesfully created your quiz! Run ${quizName}.js to play`
-                      );
-                    }
-                  });
-              }
-            );
-          }
-        }
-      );
+      // grab contents of the skeleton file
+      return Promise.all([
+        questionsData,
+        readFile(`${__dirname}/skeleton.txt`, 'utf8'),
+      ]);
+    })
+    .then(([questionsData, skeletonText]) =>
+      // populate the skeleton with our inquirer question objects
+      {
+        const populatedString = skeletonText.replace(
+          'PLACEHOLDER1',
+          JSON.stringify(questionsData)
+        );
+        // write full quiz code to file
+        return writeFile(`${process.cwd()}/${quizName}.js`, populatedString);
+      }
+    )
+    .then(() => {
+      exec(`npm i inquirer`);
+    })
+    .then(() => {
+      console.log(`Succesfully created your quiz! Run ${quizName}.js to play`);
     })
     .catch(err => {
-      console.log(err);
+      console.log({ 'Oops, looks like there was an error...': err });
     });
 }
 
